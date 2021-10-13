8f4d6bb6576e23c50fd13c53a3904e3cbc91d4ba
Crhystian Molano <crhystian.molano@mail.escuelaing.edu.co>
2021-10-12 19:36:32 -0500

    PARTE 3.

--- STOMP_WEB_SOCKETS-HTML5_JS_JAVA-WEBSOCKETS_POINTS-master/src/main/resources/static/app.js
+++ STOMP_WEB_SOCKETS-HTML5_JS_JAVA-WEBSOCKETS_POINTS-master/src/main/resources/static/app.js
@@ -8,8 +8,9 @@ var app = (function () {
     }
     
     var stompClient = null;
+    var dibujoID = null;
 
-    var addPointToCanvas = function (point) {
+	var addPointToCanvas = function (point) {
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
         ctx.beginPath();
@@ -28,7 +29,7 @@ var app = (function () {
     };
 
 
-    var connectAndSubscribe = function () {
+    var connectAndSubscribe = function (dibujoID) {
         console.info('Connecting to WS...');
         var socket = new SockJS('/stompendpoint');
         stompClient = Stomp.over(socket);
@@ -36,7 +37,7 @@ var app = (function () {
         //subscribe to /topic/TOPICXX when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-                      stompClient.subscribe('/topic/newpoint', function (eventbody) {
+                      stompClient.subscribe('/topic/newpoint/'+dibujoID, function (eventbody) {
                             let jsonObj = JSON.parse(eventbody.body);
             				//alert("Coordenadas recibidas: "+jsonObj.x+", "+jsonObj.y);   -> Parte 1
                             addPointToCanvas(new Point(jsonObj.x, jsonObj.y));
@@ -53,18 +54,27 @@ var app = (function () {
 
         init: function () {
             var can = document.getElementById("canvas");
-
-            //websocket connection
-            connectAndSubscribe();
+            dibujoID = parseInt(document.getElementById("dibujoID").value);
+            if (isNaN(dibujoID)){
+				alert("El valor debe ser un número");
+			} else {
+            	//websocket connection
+				alert("conectado a dibujo #"+dibujoID);
+				can.getContext('2d').clearRect(0, 0, 800, 600);
+            	connectAndSubscribe(dibujoID);
+			}
         },
 
         publishPoint: function(px,py){
-            var pt=new Point(px,py);
-            console.info("publishing point at "+pt);
-            
+            if (dibujoID == null) {
+            				alert("¡Conectese a un dibujo primero!");
+            			} else {
+            	            var pt=new Point(px,py);
+            	            console.info("publishing point at "+pt);
 
-            //publicar el evento
-            stompClient.send("/topic/newpoint", {}, JSON.stringify(pt));
+            	            //publicar el evento
+            				stompClient.send("/topic/newpoint/"+dibujoID, {}, JSON.stringify(pt));
+            			}
         },
 
         disconnect: function () {
